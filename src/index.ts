8f14fb84b4f9d257e9dca275a14baa3bad8d84a3
HeyChobe <00204119@uca.edu.sv>
2023-06-29 21:15:25 -0600

    :sparkles: Add accordion component

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./components/Button";
 export * from "./components/Title";
 export * from "./components/Text";
 export * from "./components/InputText";
+export * from "./components/Accordion";
 import "./global.css";
